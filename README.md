e38f295aeaae8de2cb76743aa50dd043d3e556a5
Hans Petter Bieker <hans.petter.bieker@gmail.com>
2017-01-08 20:11:17 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # homebridge-yr
+
+A homebridge temperature sensor for displaying the weather at your current location using data from yr.no.
+
+# Installation
+
+1. Install Homebridge using: `npm install -g homebridge`
+2. Install this plugin using: `npm install -g homebridge-yr`
+3. Update your Homebridge `config.json` using the sample below.
+
+# Configuration
+
+```json
+{
+  "accessory": "Yr",
+  "location": "Norge/Oslo/Oslo/Oslo",
+  "name": "Yr"
+}
+```
+
+Fields:
+
+* `accessory` must be "Yr" (required).
+* `location` location query string (resembles to <a href="http://www.yr.no/sted/Norge/Oslo/Oslo/Oslo/varsel.xml">location</a>) (required).
+* `name` is the name of the published accessory (required).
+
---
+++ index.js
@@ -0,0 +1,96 @@
+"use strict";
+
+var Service, Characteristic;
+var temperatureService;
+var request = require("request");
+
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("homebridge-yr", "Yr", WeatherAccessory);
+}
+
+function WeatherAccessory(log, config) {
+    this.log = log;
+    this.name = config["name"];
+    this.location = config["location"];
+    this.lastupdate = 0;
+    this.temperature = 0;
+}
+
+WeatherAccessory.prototype =
+    {
+        getState: function (callback) {
+            // Only fetch new data once per hour
+            if (this.lastupdate + (60 * 60) < (Date.now() / 1000 | 0)) {
+                var url = "http://www.yr.no/sted/" + this.location + "/varsel.xml";
+                this.httpRequest(url, function (error, response, responseBody) {
+                    if (error) {
+                        this.log("HTTP get weather function failed: %s", error.message);
+                        callback(error);
+                    } else {
+                        //this.log("HTTP Response", responseBody);
+                        var weatherJson = require('xml2json').toJson(responseBody);
+                        var weatherObj = JSON.parse(weatherJson);
+                        var temperature = parseFloat(weatherObj.weatherdata.forecast.tabular.time[0].temperature.value);
+                        this.log("temperature: ", temperature);
+                        this.temperature = temperature;
+                        this.lastupdate = (Date.now() / 1000);
+                        callback(null, this.temperature);
+                    }
+                }.bind(this));
+            } else {
+                this.log("Returning cached data", this.temperature);
+                temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
+                callback(null, this.temperature);
+            }
+        },
+
+        identify: function (callback) {
+            this.log("Identify requested!");
+            callback(); // success
+        },
+
+        getServices: function () {
+            var informationService = new Service.AccessoryInformation();
+
+            informationService
+                .setCharacteristic(Characteristic.Manufacturer, "Yr.no")
+                .setCharacteristic(Characteristic.Model, "Location")
+                .setCharacteristic(Characteristic.SerialNumber, "");
+
+            temperatureService = new Service.TemperatureSensor(this.name);
+            temperatureService
+                .getCharacteristic(Characteristic.CurrentTemperature)
+                .on("get", this.getState.bind(this));
+
+            temperatureService
+                .getCharacteristic(Characteristic.CurrentTemperature)
+                .setProps({minValue: -30});
+
+            temperatureService
+                .getCharacteristic(Characteristic.CurrentTemperature)
+                .setProps({maxValue: 120});
+
+            return [informationService, temperatureService];
+        },
+
+        httpRequest: function (url, callback) {
+            request({
+                    url: url,
+                    body: "",
+                    method: "GET",
+                    rejectUnauthorized: false
+                },
+                function (error, response, body) {
+                    callback(error, response, body)
+                })
+        }
+
+    };
+
+if (!Date.now) {
+    Date.now = function () {
+        return new Date().getTime();
+    }
+}
